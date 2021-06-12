d67e8d3f178430a21af9463b0238abc10d5673cc
Juan Luis Cortes Hernandez <luizhdz072@gmail.com>
2021-06-12 18:48:58 -0500

    add edite route

--- src/components/layout/routing.js
+++ src/components/layout/routing.js
@@ -5,19 +5,17 @@ import { withRouter } from "react-router-dom";
 import HomeView from "../views/home";
 import BlogView from "../views/blog";
 
-import MainContent from "./mainContent";
 import NotFound from "../views/notFound";
 
 class Routing extends React.Component {
   render() {
     return (
-      <MainContent>
         <Switch>
           <Route path={'/'} exact component={HomeView} />
           <Route path={'/blog'} exact component={BlogView} />          
+          <Route path={'/blog/:id'} exact component={BlogView} />          
           <Route path="*" component={NotFound} />
         </Switch>
-      </MainContent>
     );
   }
 }
