de803d018f75062636c69663caa6d6a2034e6e64
Juan Luis Cortes Hernandez <luizhdz072@gmail.com>
2021-06-12 21:57:08 -0500

    add styles categories

---
+++ src/components/views/category/styles.js
@@ -0,0 +1,8 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles((theme) => ({
+  divider:{
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+  }
+}));
